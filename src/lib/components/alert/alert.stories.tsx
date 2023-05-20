87a9350c029f7eebf946c84940c57bf8b022e40f
Carrie Forde <carrie@carrieforde.com>
2023-05-19 18:03:56 -0700

    update alert to work like a toaster / snackbar

--- src/lib/components/alert/alert.stories.tsx
+++ src/lib/components/alert/alert.stories.tsx
@@ -1,24 +1,63 @@
-import { Meta, StoryObj } from "@storybook/react";
-import { Alert } from "./alert";
+import { Meta, StoryFn } from "@storybook/react";
+import { Button } from "../button/button";
+import { AlertProps, AlertProvider, useAlert } from "./alert";
 
-const meta: Meta<typeof Alert> = {
+const meta: Meta<AlertProps> = {
   title: "Alert",
-  component: Alert,
 };
 
 export default meta;
 
-type Story = StoryObj<typeof Alert>;
+const Trigger = ({ variant, children, ...args }: AlertProps) => {
+  const alert = useAlert();
 
-export const Primary: Story = {
-  args: {
-    children: "This is an alert",
-  },
+  const handleClick = () => {
+    alert[variant ?? "error"](children ?? "This is an alert", args);
+  };
+
+  return (
+    <Button type="button" onClick={handleClick}>
+      Show Alert
+    </Button>
+  );
 };
 
-export const Icon: Story = {
-  ...Primary,
-  args: {
-    icon: "⚠️",
-  },
-};
+export const Error: StoryFn<AlertProps> = ({ variant = "error", ...args }) => (
+  <AlertProvider>
+    <Trigger {...args} variant={variant} />
+  </AlertProvider>
+);
+
+export const Info: StoryFn<AlertProps> = ({ variant = "info", ...args }) => (
+  <AlertProvider>
+    <Trigger {...args} variant={variant} />
+  </AlertProvider>
+);
+
+export const Success: StoryFn<AlertProps> = ({
+  variant = "success",
+  ...args
+}) => (
+  <AlertProvider>
+    <Trigger {...args} variant={variant} />
+  </AlertProvider>
+);
+
+export const Warning: StoryFn<AlertProps> = ({
+  variant = "warning",
+  ...args
+}) => (
+  <AlertProvider>
+    <Trigger {...args} variant={variant} />
+  </AlertProvider>
+);
+
+export const Icon: StoryFn<AlertProps> = ({
+  variant = "info",
+  icon = "👋",
+  ...args
+}) => (
+  <AlertProvider>
+    <Trigger {...args} variant={variant} icon={icon} />
+  </AlertProvider>
+);
--- src/lib/components/alert/alert.tsx
+++ src/lib/components/alert/alert.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import * as React from "react";
 import { createPortal } from "react-dom";
 
@@ -5,13 +7,66 @@ import cn from "classnames";
 import { Text } from "../text/text";
 import s from "./alert.module.css";
 
+type AlertVariant = "error" | "info" | "warning" | "success";
 export type AlertProps = React.PropsWithChildren<{
+  isOpen?: boolean;
   icon?: React.ReactNode;
-  variant?: "error" | "info" | "warning" | "success";
+  variant?: AlertVariant;
 }>;
 
-export const Alert: React.FC<AlertProps> = ({ variant, children, icon }) => {
+type State = AlertProps;
+
+type Action =
+  | { type: "open" }
+  | { type: "close" }
+  | {
+      type: AlertVariant;
+      payload: AlertProps;
+    };
+
+const initialState: State = {
+  isOpen: false,
+};
+
+function reducer(state: State, action: Action): State {
+  switch (action.type) {
+    case "close":
+      return { ...state, isOpen: false };
+
+    case "open":
+      return { ...state, isOpen: true };
+
+    case "error":
+    case "info":
+    case "success":
+    case "warning":
+      return {
+        ...state,
+        ...action.payload,
+        variant: action.type,
+        isOpen: true,
+      };
+
+    default:
+      return state;
+  }
+}
+
+const AlertContext = React.createContext<React.Dispatch<Action> | undefined>(
+  undefined
+);
+
+const Alert: React.FC<AlertProps> = ({
+  variant,
+  children,
+  icon,
+  isOpen = false,
+}) => {
   const alertClasses = cn(s.alert, variant && [s[variant]], "alert");
+
+  if (!isOpen) {
+    return null;
+  }
 
   return createPortal(
     <div role="alert" className={alertClasses}>
@@ -22,4 +77,84 @@ export const Alert: React.FC<AlertProps> = ({ variant, children, icon }) => {
     </div>,
     document.body
   );
+};
+
+export const AlertProvider: React.FC<React.PropsWithChildren> = ({
+  children,
+}) => {
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+
+  return (
+    <AlertContext.Provider value={dispatch}>
+      {children}
+      <Alert {...state} />
+    </AlertContext.Provider>
+  );
+};
+
+const DEFAULT_DURATION_IN_MS = 3000;
+
+type AlertOptions = Pick<AlertProps, "icon"> & {
+  duration?: number;
+};
+
+const defaultAlertOptions: AlertOptions = {
+  duration: DEFAULT_DURATION_IN_MS,
+  icon: undefined,
+};
+
+export const useAlert = () => {
+  const dispatch = React.useContext(AlertContext);
+
+  const timeout = React.useRef<any>(null);
+
+  if (!dispatch) {
+    throw new Error("`useAlert` must be called within an `AlertProvider`!");
+  }
+
+  React.useEffect(() => () => clearTimeout(timeout.current), []);
+
+  const baseAlertDispatch = React.useCallback(
+    (
+      type: AlertVariant,
+      message: React.ReactNode,
+      options: AlertOptions = defaultAlertOptions
+    ) => {
+      const { icon, duration = DEFAULT_DURATION_IN_MS } = options;
+
+      dispatch({
+        type,
+        payload: {
+          children: message,
+          icon,
+        },
+      });
+
+      timeout.current = setTimeout(() => dispatch({ type: "close" }), duration);
+    },
+    [dispatch]
+  );
+
+  return {
+    error: React.useCallback(
+      (message: React.ReactNode, options?: AlertOptions) =>
+        baseAlertDispatch("error", message, options),
+      [baseAlertDispatch]
+    ),
+    info: React.useCallback(
+      (message: React.ReactNode, options?: AlertOptions) =>
+        baseAlertDispatch("info", message, options),
+      [baseAlertDispatch]
+    ),
+    success: React.useCallback(
+      (message: React.ReactNode, options?: AlertOptions) =>
+        baseAlertDispatch("success", message, options),
+      [baseAlertDispatch]
+    ),
+    warning: React.useCallback(
+      (message: React.ReactNode, options?: AlertOptions) =>
+        baseAlertDispatch("warning", message, options),
+      [baseAlertDispatch]
+    ),
+  };
 };
